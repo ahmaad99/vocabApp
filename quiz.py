bc60604ef2cadc3df62c45af067cd3cbd3d67756
Ahmad Anim <85848892+ahmaad99@users.noreply.github.com>
2024-05-28 10:53:26 +0600

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -20,7 +20,7 @@ df2 = pd.read_csv('data/seq_ques.csv')
 if 'question_index' not in st.session_state:
     st.session_state.question_index = 0
     st.session_state.answers = []
-    st.session_state.random_numbers = [14, 150, 54, 43, 38, 102, 16, 106, 39, 17, 154, 137, 14, 146, 38, 123, 20, 59, 153, 57, 104, 16, 90, 50, 42, 84, 123, 7, 32, 154]
+    st.session_state.random_numbers = [random.randint(0, len(df[words]) - 1) for _ in range(30)]
     st.session_state.answer_index = 0
     st.session_state.correct_answers = []
     st.session_state.sentence_seq = []
